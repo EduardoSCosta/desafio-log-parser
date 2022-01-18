2e0ac55c8628d0af9f03bea480baf33ed1b93a30
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-01-18 18:12:21 -0300

    create method to count file total lines and return a json with the value

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,9 +1,11 @@
+require 'json'
+
 class LogParser
   def initialize(file_path)
     @file_path = file_path
   end
 
-  def log_reader
+  def first_line_reader
     if File.exist?(@file_path)
       file = File.open(@file_path, "r")
       begin
@@ -12,6 +14,31 @@ class LogParser
         file.close
       end
       first_line
+    else
+      raise "File not found."
+    end
+  end
+
+  def log_line_counter(file)
+    total_lines = file.readlines().size
+    total_lines
+  end
+
+  def log_file_parser
+    if File.exist?(@file_path)
+      file = File.open(@file_path, "r")
+      begin
+        total_lines = log_line_counter(file)
+      ensure
+        file.close
+      end
+      obj = {@file_path =>
+              {
+                :lines => total_lines
+              }
+            }
+      json_obj = obj.to_json
+      json_obj
     else
       raise "File not found."
     end
--- main.rb
+++ main.rb
@@ -2,4 +2,6 @@ require_relative "./lib/log_parser"
 
 file_parser = LogParser.new("games.log")
 
-puts file_parser.log_reader
+puts file_parser.first_line_reader
+
+puts file_parser.log_file_parser
