b7663b9ddb852c5e5c7d139c6df90d6cd61d8dd4
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-01-18 18:12:21 -0300

    create method to count file total lines and return a json with the value

--- spec/lib/log_parser_spec.rb
+++ spec/lib/log_parser_spec.rb
@@ -1,17 +1,27 @@
 require_relative '../../lib/log_parser'
 
-describe '#log_reader' do
+describe '#first_line_reader' do
   context 'when the file exist' do
     it 'read its first line' do
       file_parser = LogParser.new("./spec/fixtures/game_test.log")
-      expect(file_parser.log_reader).to eq("  0:00 ------------------------------------------------------------")
+      expect(file_parser.first_line_reader).to eq("  0:00 ------------------------------------------------------------")
     end
   end
 
   context 'when the file does not exist' do
     it 'raise a error message' do
       file_parser = LogParser.new("not_game_test.log")
-      expect{file_parser.log_reader}.to raise_error("File not found.")
+      expect{file_parser.first_line_reader}.to raise_error("File not found.")
+    end
+  end
+  
+end
+
+describe '#log_file_parser' do
+  context 'when the file exist' do
+    it 'read its total number of lines' do
+      file_parser = LogParser.new("./spec/fixtures/game_test.log")
+      expect(file_parser.log_file_parser).to eq("{\"./spec/fixtures/game_test.log\":{\"lines\":158}}")
     end
   end
 end
