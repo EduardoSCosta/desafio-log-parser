99d777a5b9114cb499370e5c8dc382c9bdce2a12
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-01-24 17:01:27 -0300

    create method to read file

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -16,35 +16,38 @@ class LogParser
   end
   
   def log_file_parser
-    file = file_opener
-    begin
-      json_file = json_generator(file)
-    ensure
-      file.close
-    end
+    json_file = json_generator
     json_file
   end
 
   private
 
   def file_opener
-    if File.exist?(@file_path)
-      file = File.open(@file_path, "r")
-      file
-    else
-      raise "File not found."
+    raise "File not found." unless File.exist?(@file_path)
+
+    file = File.open(@file_path, "r")
+    file
+  end
+
+  def file_reader
+    file = file_opener
+    begin
+      file_lines = file.readlines
+    ensure
+      file.close
     end
+    file_lines
   end
 
-  def log_line_counter(file)
-    total_lines = file.readlines().size
-    total_lines
+  def log_line_counter
+    file_size = file_reader.size
+    file_size
   end
 
-  def json_generator(file)
+  def json_generator
     obj = {
       @file_path => {             
-        :lines => log_line_counter(file)
+        :lines => log_line_counter
       }
     }
     json_obj = obj.to_json
